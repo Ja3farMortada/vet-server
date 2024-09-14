1362e8711eddab2b3f4b2d7aa46813a046eaeed8
JaafarMortada <mortada9190@gmail.com>
2024-09-14 19:05:37 +0300

    fix a stuff

--- config/database.js
+++ config/database.js
@@ -58,24 +58,24 @@ if (process.env.NODE_ENV === "production") {
 		});
 	});
 } else {
-	var pool = mysql.createPool({
-		connectionLimit: 10,
-		host: "localhost",
-		user: "root",
-		password: "roottoor",
-		database: "vet",
-		multipleStatements: true,
-		dateStrings: true,
-	});
 	// var pool = mysql.createPool({
 	// 	connectionLimit: 10,
-	// 	host: "66.29.148.84",
-	// 	user: "ivkqkoujku_vet",
-	// 	password: "5~q^ukf0iIU4",
-	// 	database: "ivkqkoujku_vet",
+	// 	host: "localhost",
+	// 	user: "root",
+	// 	password: "roottoor",
+	// 	database: "vet",
 	// 	multipleStatements: true,
 	// 	dateStrings: true,
 	// });
+	var pool = mysql.createPool({
+		connectionLimit: 10,
+		host: "localhost",
+		user: "ivkqkoujku_vet",
+		password: "5~q^ukf0iIU4",
+		database: "ivkqkoujku_vet",
+		multipleStatements: true,
+		dateStrings: true,
+	});
 }
 
 pool.getConnection(async (err, connection) => {
