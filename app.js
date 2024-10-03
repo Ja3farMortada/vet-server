c122875dfdfc7ea2fe4fb03595b1d2cffc6999ad
Jeff <mortada9190@gmail.com>
2024-10-03 16:57:05 +0300

    update https

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const fs = require("fs");
+const https = require("https");
+
 const server = require("./index");
 
-server.listen(3500, () => console.log(`listening on port 3500 ...`));
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/your_domain.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/your_domain.com/fullchain.pem"),
+};
+// server.listen(3500, () => console.log(`listening on port 3500 ...`));
+// const PORT = 443; // HTTPS port
+https.createServer(options, 3500).listen(PORT, () => {
+    console.log(`Server running at https://vet101.online`);
+});
