98fd9c2d7b815179cd67442bd8a25957cc62d131
Jeff <mortada9190@gmail.com>
2024-10-03 17:11:19 +0300

    update

--- pm2.js
+++ pm2.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "vet-app",
-        script: "./app.js",  // Replace with your entry point
+        script: "app.js",  // Replace with your entry point
         env: {
           NODE_ENV: "production",
           // Add other environment variables here
