522aa06e3ddc44580927c7ce68084e01b6c8514a
Jeff <mortada9190@gmail.com>
2024-11-16 15:51:19 +0200

    update socket

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const app = require("./index");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json());
+const path = require("path");
+
+// allow Cross-Origin calls to this app
+const cors = require("cors");
+app.use(cors());
+
+const { auth, admin } = require("./middleware/auth");
+const errorHandler = require("./middleware/errorHandler");
 
 const fs = require("fs");
 const http = require("http");
@@ -31,4 +42,48 @@ app.use((req, res, next) => {
   req.io = io;
   next();
 });
+
+//import routes
+const AuthRoutes = require("./routes/auth.routes");
+const UsersRoutes = require("./routes/users.routes");
+const CustomersRoutes = require("./routes/customers.routes");
+const SuppliersRoutes = require("./routes/suppliers.routes");
+const StockRoutes = require("./routes/stock.routes");
+const ProfileRoutes = require("./routes/profile.routes");
+const SellOrdersRoutes = require("./routes/sell-orders.routes");
+const HistoryRoutes = require("./routes/history.routes");
+const PaymentRoutes = require("./routes/payment.routes");
+const BalanceRoutes = require("./routes/balance.routes");
+const ExpenseRoutes = require("./routes/expense.routes");
+const ReportRoutes = require("./routes/report.routes");
+const ReturnRoutes = require("./routes/return.routes");
+const ReservationsRoutes = require("./routes/reservations.routes");
+const PetsRoutes = require("./routes/pets.routes");
+
+// common routes
+app.use("/auth", AuthRoutes);
+app.use("/customers", auth, CustomersRoutes);
+app.use("/profile", auth, ProfileRoutes);
+app.use("/sell-orders", auth, SellOrdersRoutes);
+app.use("/history", auth, HistoryRoutes);
+app.use("/payment", auth, PaymentRoutes);
+app.use("/balance", auth, BalanceRoutes);
+app.use("/expense", auth, ExpenseRoutes);
+app.use("/report", auth, ReportRoutes);
+app.use("/return", auth, ReturnRoutes);
+app.use("/suppliers", auth, SuppliersRoutes);
+app.use("/stock", StockRoutes);
+app.use("/reservations", auth, ReservationsRoutes);
+app.use("/pets", auth, PetsRoutes);
+
+// admin routes
+app.use("/users", admin, UsersRoutes);
+
+// check API status page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// handle errors
+app.use(errorHandler);
 server.listen(443, () => console.log(`listening on port 443 ...`));
--- routes/report.routes.js
+++ routes/report.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const ReportController = require("../controllers/ReportController");
 
 router.get("/total-sales/:start/:end", ReportController.getTotalSales);
+// router.get("/top-sales/:start&:end&:id", ReportController.getTopSales);
 
 module.exports = router;
