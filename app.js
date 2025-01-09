a950413d6e80bf65421be522591de5c0deb04c03
Jaafar Mortada <mortada9190@gmail.com>
2025-01-09 20:43:09 +0200

    add app file

--- app.js
+++ app.js
@@ -16,16 +16,14 @@ const http = require("http");
 // Redirect HTTP to HTTPS
 
 let options;
-http
-  .createServer((req, res) => {
-    res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-    res.end();
-  })
-  .listen(80);
+http.createServer((req, res) => {
+	res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+	res.end();
+}).listen(80);
 
 options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/vet101.online/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/vet101.online/fullchain.pem"),
+	key: fs.readFileSync("/etc/letsencrypt/live/vet101.online/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/vet101.online/fullchain.pem"),
 };
 // create server for socket.io
 const https = require("https");
@@ -34,13 +32,13 @@ const server = https.createServer(options, app); // Create server from Express a
 
 const socketIO = require("socket.io");
 const io = socketIO(server, {
-  cors: {
-    origins: ["*"],
-  },
+	cors: {
+		origins: ["*"],
+	},
 });
 app.use((req, res, next) => {
-  req.io = io;
-  next();
+	req.io = io;
+	next();
 });
 
 //import routes
@@ -83,7 +81,13 @@ app.use("/users", admin, UsersRoutes);
 
 // check API status page
 app.get("/api", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+	res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// launch app
+app.use(express.static(path.join(__dirname, "app/browser")));
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "app/browser", "index.html"));
 });
 
 // handle errors
