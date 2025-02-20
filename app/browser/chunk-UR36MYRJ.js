62aca1dd086fc6e10b1627f55813b8f4efd3b47b
Jaafar Mortada <mortada9190@gmail.com>
2025-02-20 14:35:05 +0200

    updates app

--- app/browser/chunk-UR36MYRJ.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./chunk-VBDHWICU.js";import{b as c,d as i}from"./chunk-BDTGQIJB.js";import{Ab as o,Gc as l,K as a,P as r,Rb as t,fc as n}from"./chunk-WLCZSOBW.js";var ae=(()=>{class e{static \u0275fac=function(p){return new(p||e)};static \u0275mod=r({type:e});static \u0275inj=a({imports:[o,t,n,l,i,s,c,t,n,i]})}return e})();export{ae as a};
---
+++ app/browser/chunk-V4MCNO6K.js
@@ -0,0 +1,1 @@
+import{a as s}from"./chunk-P3OYWVPG.js";import{b as c,d as i}from"./chunk-ULBQ4ARH.js";import{Ab as o,Gc as l,K as a,P as r,Rb as t,fc as n}from"./chunk-UKD27P7M.js";var ae=(()=>{class e{static \u0275fac=function(p){return new(p||e)};static \u0275mod=r({type:e});static \u0275inj=a({imports:[o,t,n,l,i,s,c,t,n,i]})}return e})();export{ae as a};
